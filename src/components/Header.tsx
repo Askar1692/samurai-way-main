caf1ac67d9af663883a2a51f3809560d3be0b65d
Askar1692 <askar.kusemisov@gmail.com>
2022-05-25 14:25:28 +0600

    samurai-way-12

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCZlf5lc5tX-0gY-y94pGS0mQdL-D0lCH2OQ&usqp=CAU"/>
+        </header>
+    );
+}
